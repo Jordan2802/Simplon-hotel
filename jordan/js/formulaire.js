cbf6308a6e61f9944a9125a377be7d1c79413183
jordan2802 <sha08dow@hotmail.fr>
2018-06-08 12:31:01 +0200

    hfg

--- jordan/js/formulaire.js
+++ jordan/js/formulaire.js
@@ -12,3 +12,45 @@ $(window).scroll(function (event) {
     $('nav').removeClass('fixed');
   }
 });
+
+
+/*verif formulaire remplis*/
+
+
+function vide()
+{
+    var prenom = document.getElementById ('prenom').value;
+    var nom = document.getElementById ('nom').value;
+    var homme = document.getElementById ('homme').checked;
+    var femme = document.getElementById ('femme').checked;
+    var email = document.getElementById ('email').value;
+    var tel = document.getElementById ('tel').value;
+    if (homme =="" && femme =="")
+    {
+        alert('veuillez renseigner votre genre !');
+        return false;
+    }
+    if(nom=="")
+    {
+        alert('veuillez renseigner votre nom !');
+        document.getElementById('nom').focus();
+        return false;
+    }
+    if(prenom=="")
+    {
+        alert('veuillez renseigner votre prenom !');
+        document.getElementById('prenom').focus();
+        return false;
+    }
+    if(email=="")
+    {
+        alert('veuillez renseigner votre adresse email !');
+        document.getElementById('prenom').focus();
+        return false;
+    }
+
+
+
+    else
+        return true;
+}
